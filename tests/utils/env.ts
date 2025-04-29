9bc62ce2398a5f4d653e0d15841ca44189c5a546
Stepan Protsak <stephan.protsack@gmail.com>
2025-04-29 14:37:40 +0200

    Add assestion for AUTH_TOKEN

--- tests/utils/env.ts
+++ tests/utils/env.ts
@@ -14,4 +14,8 @@ export async function setup() {
   if (!process.env.USER_PASSWORD) {
     throw new Error("USER_PASSWORD is not defined");
   }
+
+  if (!process.env.AUTH_TOKEN) {
+    throw new Error("AUTH_TOKEN is not defined");
+  }
 }
