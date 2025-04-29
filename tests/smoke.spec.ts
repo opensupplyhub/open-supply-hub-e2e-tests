9026a37c8f32c157c5c0ffe7a98b3c31ec78c298
nazarkolossandf <nazar.kolos@speedandfunction.com>
2025-04-29 14:41:26 +0300

    Basic search first commit

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -126,3 +126,37 @@ test.describe("OSDEV-1233: Smoke: API. Search for valid facilities through an en
     expect(response.status()).toBe(401);
   });
 });
+
+test("OSDEV-1232: Facilities. Invalid search parameters",  async ({ page }) => {
+  const { BASE_URL } = process.env;
+  await page.goto(BASE_URL!);
+
+  const invalidSearchQuery = "invalid ABRACADABRA";
+  await page.getByPlaceholder("e.g. ABC Textiles Limited").click();
+  await page
+    .getByPlaceholder("e.g. ABC Textiles Limited")
+    .fill(invalidSearchQuery); 
+  
+  await page.getByRole("button", { name: "Find Facilities" }).click();
+  await page.waitForResponse(
+    async (resp) => resp.url().includes(`api/facilities/?q=`) && resp.status() == 200
+  );
+  await expect(page.getByText("No facilities matching this")).toBeVisible();
+});
+
+test("OSDEV-1232: Facilities. Valid search parameters", async ({ page }) => {
+  const { BASE_URL } = process.env;
+
+  await page.goto(BASE_URL!);
+   const validSearchQuery = "coffee factory";
+  await page.getByPlaceholder("e.g. ABC Textiles Limited").click();
+  await page
+    .getByPlaceholder("e.g. ABC Textiles Limited")
+    .fill(validSearchQuery);
+  await page.getByRole("button", { name: "Find Facilities" }).click();
+  await page.waitForResponse(
+    async (resp) => resp.url().includes("api/facilities/?q=") && resp.status() == 200
+  );
+  await expect(page.getByText("# Contributors")).toBeVisible();
+});
+
