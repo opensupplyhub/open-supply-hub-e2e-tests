60d4d90aaaf62b14e7384cf5244a94cae205de8d
Inessa Druzhkova <inessadruzkova@inessas-mbp.home>
2025-05-19 16:58:45 +0200

    replaced XLSX with ExcelJS

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { setup } from "./utils/env";
 import { get } from "./utils/api";
+import * as path from "path";
+import * as fs from "fs";
+import ExcelJS from "exceljs";
 
 test.beforeAll(setup);
 
@@ -126,4 +129,110 @@ test.describe("OSDEV-1233: Smoke: API. Search for valid facilities through an en
     });
     expect(response.status()).toBe(401);
   });
+});
+
+test.describe("OSDEV-1264: Smoke: Download a list of facilities with amounts 7000 - 9900 in xlsx.", async () => {
+  test("An unauthorized user cannot download a list of facilities.", async ({
+    page,
+  }) => {
+    const { BASE_URL } = process.env;
+    await page.goto(`${BASE_URL}/facilities/?countries=AO&countries=BE&countries=PL&sort_by=contributors_desc`!);
+
+    const title = await page.title();
+    expect(title).toBe("Open Supply Hub");
+    await page.waitForLoadState("networkidle");
+
+    const downloadButton = page.getByRole("button", { name: "Download" });
+    expect(downloadButton).toBeVisible();
+    expect(downloadButton).toBeEnabled();
+    downloadButton.click({ force: true });
+
+    await page.waitForLoadState("networkidle");
+
+    const menuItem = page.getByRole("menuitem", { name: "Excel" });
+    await expect(menuItem).toBeVisible();
+    menuItem.click({ force: true });
+
+    await expect(page.getByRole("heading", { name: "Log In To Download" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "CANCEL" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "REGISTER" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "LOG IN" })).toBeVisible();
+  })
+
+  test("An authorized user can download a list of facilities with amounts 7000 - 9900 in xlsx.", async ({
+    page,
+  }) => {
+    const { BASE_URL } = process.env;
+    await page.goto(`${BASE_URL}/facilities/?countries=AO&countries=BE&countries=PL&sort_by=contributors_desc`!);
+
+    page.getByRole("button", { name: "Download" }).click({ force: true });
+
+    const menuItem = page.getByRole("menuitem", { name: "Excel" });
+    await expect(menuItem).toBeVisible();
+
+    menuItem.click({ force: true });
+
+    page.getByRole("button", { name: "LOG IN" }).click();
+
+    const { USER_EMAIL, USER_PASSWORD } = process.env;
+    await page.getByLabel("Email").fill(USER_EMAIL!);
+    await page.getByRole("textbox", { name: "Password" }).fill(USER_PASSWORD!);
+    await page.getByRole("button", { name: "Log In" }).click();
+
+    page.getByRole("button", { name: "Download" }).click({ force: true });
+    page.getByRole("menuitem", { name: "Excel" }).click({ force: true });
+
+    const downloadPath = path.resolve(__dirname, "downloads");
+    const downloadPromise = page.waitForEvent("download");
+
+    const download = await downloadPromise;
+    const filePath = path.join(downloadPath, download.suggestedFilename());
+
+    await download.saveAs(filePath);
+
+    const fileExists = fs.existsSync(filePath);
+    expect(fileExists).toBe(true);
+
+    const fileName = path.basename(filePath);
+    expect(fileName).toContain("facilities");
+    expect(fileName).toContain(".xlsx");
+
+    async function readXlsx(filePath: string) {
+      const fileName = path.basename(filePath);
+      if (!fileName.endsWith(".xlsx")) throw new Error("Invalid file type");
+
+      const workbook = new ExcelJS.Workbook();
+      await workbook.xlsx.readFile(filePath);
+
+      const rows: any[][] = [];
+      if (workbook.worksheets.length === 0) {
+        throw new Error("Workbook contains no sheets");
+      }
+
+      const worksheet = workbook.worksheets[0];
+      if (!worksheet) {
+        throw new Error("First worksheet is null");
+      }
+
+      worksheet.eachRow((row: any) => {
+        if (row.values && Array.isArray(row.values)) {
+          rows.push(row.values.slice(1));
+        }
+      });
+
+      return rows;
+    }
+
+    const results = page.getByText(/^\d+ results$/);
+    await expect(results).toBeVisible();
+
+    // Get count of Facilities output on the  UI
+    const text = await results.textContent();
+    const numberOfFacilities = parseInt(text?.match(/\d+/)?.[0] || "0", 10);
+    const headerRow = 1;
+
+    await readXlsx(filePath).then(rows => {
+      expect(rows.length).toEqual(numberOfFacilities + headerRow);
+    });
+  })
 });
