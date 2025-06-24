d46abe0087f832ebe247accf2aadb4d039cf8905
nazarkolossandf <nazar.kolos@speedandfunction.com>
2025-06-24 14:10:27 +0300

    Added checks for old API Get Endpoints (#20)
    * added checks for get endpoints
    
    * Update formatting
    
    ---------
    
    Co-authored-by: Stepan Protsak <stephan.protsack@gmail.com>

---
+++ tests/schemas/parent-companies-schema.ts
@@ -0,0 +1,12 @@
+export const parentCompanies = {
+  type: "array",
+  items: {
+    type: "array",
+    minItems: 2,
+    maxItems: 2,
+    items: [
+      { anyOf: [{ type: "integer" }, { type: "string" }] },
+      { type: "string" },
+    ],
+  },
+};
---
+++ tests/schemas/sectors-schema.ts
@@ -0,0 +1,6 @@
+export const sectors = {
+  type: "array",
+  items: {
+    type: "string",
+  },
+};
