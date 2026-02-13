1a3df9d13f2f8f937f2fceb275db9cec04c2fa6d
Anna Bondar <a.bondarenko1000@gmail.com>
2026-02-13 18:40:11 +0100

    Uncommented  skipping for non-prod envs

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1811,9 +1811,9 @@ test.describe("OSDEV-1264: Smoke: Download a list of facilities with amounts up 
 });
 
 test.describe("OSDEV-1275: Smoke: EM user can see embedded map working properly at their websites.", () => {
-  // if (process.env.ENVIRONMENT !== "production") {
-  //   test.skip(true, "Only runs in Production environment");
-  // }
+  if (process.env.ENVIRONMENT !== "production") {
+    test.skip(true, "Only runs in Production environment");
+  }
 
   // Company name = link to the site
   const linksToSitesWhereCheckEM = [
